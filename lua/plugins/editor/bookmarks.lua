a096ff95ad5eca579de1acc23bb9a955a4296d40
x...x <someone2011@foxmail.com>
2025-10-14 11:15:14 -0500

    add bookmarks tutorial

--- lua/plugins/editor/bookmarks.lua
+++ lua/plugins/editor/bookmarks.lua
@@ -11,18 +11,15 @@
 
 return {
 	'crusj/bookmarks.nvim',
-	keys = {
-		{ 'mm', mode = { 'n' } },
-		{ '<leader>ma', mode = { 'n' } },
-		{ '<leader>ml', mode = { 'n' } },
-		{ '<leader>mc', mode = { 'n' } },
-		{ ']b', mode = { 'n' } },
-		{ '[b', mode = { 'n' } },
-	},
+	-- Load on VimEnter to ensure it's always available
+	event = 'VimEnter',
 	branch = 'main',
-	dependencies = { 'nvim-web-devicons' },
+	dependencies = { 'nvim-web-devicons', 'nvim-telescope/telescope.nvim' },
 	config = function()
-		require('bookmarks').setup {
+		local bookmarks = require 'bookmarks'
+
+		-- Setup bookmarks
+		bookmarks.setup {
 			-- Save bookmarks in memory only (not persisted to disk)
 			-- They disappear when you close Neovim - perfect for temporary workflow
 			save_file = vim.fn.stdpath 'data' .. '/bookmarks',
@@ -35,7 +32,7 @@ return {
 			on_attach = function(bufnr)
 				local bm = require 'bookmarks'
 				local map = vim.keymap.set
-				-- Toggle bookmark at current line (mm = mark mark)
+				-- Toggle bookmark at current line
 				map('n', 'mm', bm.bookmark_toggle, { desc = 'Toggle bookmark', buffer = bufnr })
 				-- Jump to next/previous bookmark
 				map('n', ']b', bm.bookmark_next, { desc = 'Next bookmark', buffer = bufnr })
@@ -47,6 +44,16 @@ return {
 				map('n', '<leader>mc', bm.bookmark_clean, { desc = '[C]lear bookmarks in buffer', buffer = bufnr })
 			end,
 		}
+
+		-- Load Telescope extension
 		require('telescope').load_extension 'bookmarks'
+
+		-- Set up global keymaps (in case on_attach doesn't work)
+		vim.keymap.set('n', 'mm', bookmarks.bookmark_toggle, { desc = 'Toggle bookmark' })
+		vim.keymap.set('n', ']b', bookmarks.bookmark_next, { desc = 'Next bookmark' })
+		vim.keymap.set('n', '[b', bookmarks.bookmark_prev, { desc = 'Previous bookmark' })
+		vim.keymap.set('n', '<leader>ma', '<cmd>Telescope bookmarks list<cr>', { desc = 'Show [a]ll bookmarks' })
+		vim.keymap.set('n', '<leader>ml', '<cmd>Telescope bookmarks list<cr>', { desc = '[L]ist bookmarks' })
+		vim.keymap.set('n', '<leader>mc', bookmarks.bookmark_clean, { desc = '[C]lear bookmarks in buffer' })
 	end,
 }
